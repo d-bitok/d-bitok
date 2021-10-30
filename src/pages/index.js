72c0afe5580ad803bdbf65410fd715822ae1f6e0
rexdougie <bitokdouglas@gmail.com>
2021-10-30 13:00:47 +0100

    Files And Directories Added

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit quaerat quod omnis ducimus, corrupti accusamus nisi eum officiis dignissimos suscipit aliquam eligendi unde, dolorum magnam eius. Officiis eveniet error libero.
+        </p>
+    )
+}
