89cbec1b4e2712538ffeb0c0509a54603fab3954
Pheon-Dev <devpheon@gmail.com>
2023-08-30 15:09:02 +0300

    [+] optimisation for build

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,63 +1,81 @@
 export const projects = [
-    {
-        id: 0,
-        title: 'C&D Tech',
-        description: "Our company's official services rendered with detailed description of each speciality is listed on this website. Requests can be made by customers as well in realtime.",
-        image: '/images/cnd.jpg',
-        tags: ['Vue JS', 'Firebase', 'GitHub'],
-        github: 'https://github.com/rexdougie/cnd-tech',
-        visit: 'https://cnd-tech.web.app/',
-    },
-    {
-        id: 1,
-        title: 'Wavma Security',
-        description: "WAVMA SECURITY COMPANY LIMITED(WSCL) is a duly incorporated and registered under the companies Act, laws of Kenya. As a limited company, it transacts business as laid down in the companies Act Chapter 486 laws of Kenya.",
-        image: '/images/wavma-sec.jpg',
-        tags: ['HTML5', 'CSS', 'Bootstrap', 'Vanilla JS', 'Firebase'],
-        github: 'https://github.com/rexdougie/wavma',
-        visit: 'https://wavma-security.web.app/',
-    },
-    {
-        id: 2,
-        title: 'Prex Interiors',
-        description: "They specialize in commercial, residential, retail and office construction and interior design.They also offer Architectural drawings and quality 3D renders.",
-        image: '/images/prex.jpg',
-        tags: ['Vanilla JS', 'Firebase', 'HTML5', 'CSS'],
-        github: 'https://github.com/rexdougie/prex',
-        visit: 'https://prex-interiors-ltd.web.app/',
-    },
-    {
-        id: 3,
-        title: 'Cryptapp',
-        description: "A website that shows all live information on crypto markets, listing top ten crypto currencies, their stats and latest news. ",
-        image: '/images/cryptapp.jpg',
-        tags: ['React JS', 'Node JS', 'RapidApi', 'Firebase'],
-        github: 'https://github.com/rexdougie/cryptapp',
-        visit: 'https://cryptapp-41c50.web.app/',
-    },
-    {
-        id: 4,
-        title: 'Kenya Prisons',
-        description: "Kenya Prisons Sports Volleyball Club of Kenya. Events, tournaments and championships along with the results and players involved are shown.",
-        image: '/images/prisons.jpg',
-        tags: ['Vanilla JS', 'HTML5', 'Bootstrap', 'Firebase'],
-        github: 'https://github.com/rexdougie/',
-        visit: 'https://kenya-prisons-sports.web.app/',
-    },
-    {
-        id: 5,
-        title: 'Kikao64 Cafe',
-        description: "An inbuilt cafe system for Kikao64 where customers can make orders based on the menu and be delivered to their respective tables.",
-        image: '/images/kikao64.jpg',
-        tags: ['React Native', 'React', , 'Firebase'],
-        github: 'https://github.com/rexdougie/kikao64',
-        visit: 'https://kikao64.web.app/',
-    },
-]
+  {
+    id: 0,
+    title: "Destiny Credit LTD",
+    description:
+      "A loan management company that offers loans to individuals and businesses. The system is integrated with M-PESA and pamynets are reflected in realtime which can be used to monitor a clients loan repayment",
+    image: "/images/destiny.jpg",
+    tags: ["Next JS", "Vercel", "GitHub"],
+    github: "https://github.com/Pheon-Dev/destiny-credit",
+    visit: "https://destiny-credit.vercel.app/",
+  },
+  {
+    id: 1,
+    title: "Wavma Security",
+    description:
+      "WAVMA SECURITY COMPANY LIMITED(WSCL) is a duly incorporated and registered under the companies Act, laws of Kenya. As a limited company, it transacts business as laid down in the companies Act Chapter 486 laws of Kenya.",
+    image: "/images/wavma-sec.jpg",
+    tags: ["HTML5", "CSS", "Bootstrap", "Vanilla JS", "Firebase"],
+    github: "https://github.com/d-bitok/wavma",
+    visit: "https://wavma-security.web.app/",
+  },
+  {
+    id: 2,
+    title: "Prex Interiors",
+    description:
+      "They specialize in commercial, residential, retail and office construction and interior design.They also offer Architectural drawings and quality 3D renders.",
+    image: "/images/prex.jpg",
+    tags: ["Vanilla JS", "Firebase", "HTML5", "CSS"],
+    github: "https://github.com/d-bitok/prex",
+    visit: "https://prex-interiors-ltd.web.app/",
+  },
+  {
+    id: 3,
+    title: "Cryptapp",
+    description:
+      "A website that shows all live information on crypto markets, listing top ten crypto currencies, their stats and latest news. ",
+    image: "/images/cryptapp.jpg",
+    tags: ["React JS", "Node JS", "RapidApi", "Firebase"],
+    github: "https://github.com/d-bitok/cryptapp",
+    visit: "https://cryptapp-41c50.web.app/",
+  },
+  {
+    id: 4,
+    title: "Kenya Prisons",
+    description:
+      "Kenya Prisons Sports Volleyball Club of Kenya. Events, tournaments and championships along with the results and players involved are shown.",
+    image: "/images/prisons.jpg",
+    tags: ["Vanilla JS", "HTML5", "Bootstrap", "Firebase"],
+    github: "https://github.com/d-bitok/",
+    visit: "https://kenya-prisons-sports.web.app/",
+  },
+  {
+    id: 5,
+    title: "Kikao64 Cafe",
+    description:
+      "An inbuilt cafe system for Kikao64 where customers can make orders based on the menu and be delivered to their respective tables.",
+    image: "/images/kikao64.jpg",
+    tags: ["React Native", "React", , "Firebase"],
+    github: "https://github.com/d-bitok/kikao64",
+    visit: "https://kikao64.web.app/",
+  },
+  {
+    id: 6,
+    title: "C&D Tech",
+    description:
+      "Our company's official services rendered with detailed description of each speciality is listed on this website. Requests can be made by customers as well in realtime.",
+    image: "/images/cnd.jpg",
+    tags: ["Vue JS", "Firebase", "GitHub"],
+    github: "https://github.com/d-bitok/cnd-tech",
+    visit: "https://cnd-tech.web.app/",
+  },
+];
 
 export const TimeLineData = [
-    { year: 2019, text: 'Started my journey' },
-    { year: 2020, text: 'Worked On Petty Personal Projects to Hone My Skills' },
-    { year: 2021, text: 'Worked With Different Companies' },
-    { year: 2022, text: 'The Journey Continues ...' },
-]
+  { year: 2019, text: "Started my journey" },
+  { year: 2020, text: "Worked On Petty Personal Projects to Hone My Skills" },
+  { year: 2021, text: "Worked With Different Companies" },
+  { year: 2022, text: "Built a Loan Management System" },
+  { year: 2023, text: "Worked on an API system for money transfer" },
+  { year: 2024, text: "The Journey Continues ..." },
+];
